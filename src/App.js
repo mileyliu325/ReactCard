5737ad9619d0cb97e5149cd9595a1c3fc9a3d023
Miley Liu <smileyliu325@gamil.com>
2019-01-09 13:28:58 +1100

    format

--- src/App.js
+++ src/App.js
@@ -4,10 +4,17 @@ import "./App.css";
 import "./bootstrap.css";
 import Card from "./Card";
 
-const title = ['Seer','Witch','Villager'];
-const subtitle = ['Could know character\'s role','could save one and poision one ','Could vote'];
-const image = ['http://ww4.sinaimg.cn/large/006tT5Uqjw1f4xtwynftlj3066066mxp.jpg','http://ww3.sinaimg.cn/large/006tT5Uqjw1f4xtx8e15aj3066066js3.jpg','http://ww1.sinaimg.cn/large/006tT5Uqjw1f4xtw9lzwqj306a06a0tb.jpg']
-
+const title = ["Seer", "Witch", "Villager"];
+const subtitle = [
+  "Could know character's role",
+  "could save one and poision one ",
+  "Could vote"
+];
+const image = [
+  "http://ww4.sinaimg.cn/large/006tT5Uqjw1f4xtwynftlj3066066mxp.jpg",
+  "http://ww3.sinaimg.cn/large/006tT5Uqjw1f4xtx8e15aj3066066js3.jpg",
+  "http://ww1.sinaimg.cn/large/006tT5Uqjw1f4xtw9lzwqj306a06a0tb.jpg"
+];
 
 class App extends Component {
   render() {
@@ -17,16 +24,15 @@ class App extends Component {
         <br />
         <div class="row container-fluid">
           <div class="col-sm-4 col-md-4 col-lg-4 text-center align-self-center">
-            <Card img={image[0]} title={title[0]} subtitle={subtitle[0]}/>
+            <Card img={image[0]} title={title[0]} subtitle={subtitle[0]} />
           </div>
 
           <div class="col-sm-4 col-md-4 col-lg-4 text-center align-self-center">
-          <Card img={image[1]} title={title[1]} subtitle={subtitle[1]}/> 
-         
+            <Card img={image[1]} title={title[1]} subtitle={subtitle[1]} />
           </div>
 
           <div class="col-sm-4 col-md-4 col-lg-4 text-center align-self-center">
-          <Card img={image[2]} title={title[2]} subtitle={subtitle[2]}/>
+            <Card img={image[2]} title={title[2]} subtitle={subtitle[2]} />
           </div>
         </div>
       </div>
